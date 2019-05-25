84b823d615e865b12d35f06cb038a369e9d7f6e9
Khamitsevich Mikhail <mikhail.khamitsevich@sap.com>
2019-05-25 19:54:05 +0000

    server is ok

---
+++ srv/server.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const https = require("https");
+const port = process.env.PORT || 3000;
+const server = require("http").createServer();
+const cds = require("@sap/cds");
+const xsenv = require("@sap/xsenv");
+const xsHDBConn = require("@sap/hdbext");
+const express = require("express");
+
+https.globalAgent.options.ca = xsenv.loadCertificates();
+global.__base = __dirname + "/";
+
+const app = express();
+
+var hanaOptions = xsenv.getServices({
+	hana: {
+		tag: "hana"
+	}
+});
+
+hanaOptions.hana.pooling = true;
+
+app.use(
+    xsHDBConn.middleware(hanaOptions.hana)
+);
+
+cds.connect({
+    kind: "hana",
+    logLevel: "error",
+    credentials: hanaOptions.hana
+});
+
+cds.serve("gen/csn.json", {
+        crashOnError: false
+    })
+    .at("/odata/v4/")
+    .in(app)
+    .catch(err => {
+        // do not crash on error ???
+        console.log(err);
+		process.exit(1);
+    });
+    
+require("./router")(app, server);
+
+server.on("request", app);
+server.listen(port, () => console.info(`HTTP Server: ${server.address().port}`));
